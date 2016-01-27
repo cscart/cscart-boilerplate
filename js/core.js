3373d802c6ffa6a5b15af44e4bffa7a9bee5c885
amelekesov <i198622@gmail.com>
2016-01-27 13:16:34 +0300

    ceTooltip init were removed.

--- js/core.js
+++ js/core.js
@@ -975,22 +975,6 @@ var Tygh = {
                 }
             }
 
-            // FIXME: Backward compatibility
-            if ($('#push').length > 0) {
-                // StickyFooter
-                $.stickyFooter();
-            }
-
-            // init stickyScroll plugin
-            $('.cm-sticky-scroll').ceStickyScroll();
-
-            $(_.doc).on('mouseover', '.cm-tooltip[title]', function() {
-                if (!$(this).data('tooltip')) {
-                    $(this).ceTooltip();
-                }
-                $(this).data('tooltip').show();
-            });
-
             // auto open dialog
             var dlg = $('.cm-dialog-auto-open');
             dlg.ceDialog('open', $.ceDialog('get_params', dlg));
@@ -3285,69 +3269,6 @@ var Tygh = {
 
     /*
      *
-     * Tooltips
-     *
-     */
-     (function($){
-
-        var methods = {
-
-            init: function(params) {
-
-                var default_params = {
-                    events: {
-                        def: 'mouseover, mouseout',
-                        input: 'focus, blur'
-                    },
-                    layout: '<div><span class="tooltip-arrow"></span></div>'
-                };
-
-                $.extend(default_params, params);
-
-                return this.each(function() {
-                    var elm = $(this);
-                    var params = default_params;
-
-                    if (elm.data('tooltip')) {
-                        return false;
-                    }
-
-                    if (elm.data('ceTooltipPosition') === 'top') {
-                        params.position = 'top left';
-                        params.tipClass = 'tooltip arrow-top';
-                        params.offset = [-10, 7];
-                    } else {
-                        params.offset = [10, 7];
-                        params.tipClass = 'tooltip arrow-down';
-                        params.position = 'bottom left';
-                    }
-
-                    elm.tooltip(params).dynamic({
-                        right: {},
-                        left: {}
-                    });
-
-                    //hide tooltip before remove
-                    elm.on("remove", function() {
-                        $(this).trigger('mouseout');
-                    });
-                });
-            }
-        };
-
-        $.fn.ceTooltip = function(method) {
-            if (methods[method]) {
-                return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-            } else if ( typeof method === 'object' || ! method ) {
-                return methods.init.apply(this, arguments);
-            } else {
-                $.error('ty.tooltip: method ' +  method + ' does not exist');
-            }
-        };
-    })($);
-
-    /*
-     *
      * Sortables
      *
      */
@@ -4324,7 +4245,7 @@ var Tygh = {
 
                     var notification = $('<div class="cm-notification-content notification-content ' + n_class + '" data-ca-notification-key="' + key + '">' +
                         '<button type="button" class="close cm-notification-close ' + b_class + '" data-dismiss="alert">&times;</button>' +
-                        '<strong>' + data.title + '</strong>' + data.message +
+                        '<strong>' + data.title + '</strong> ' + data.message +
                         '</div>');
 
                     if (!_addToDialog(notification)) {
